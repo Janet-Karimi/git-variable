bf73b6d3756c79c25dfd8c5b17049d40deee5d4e
Janet <janetkarimithuranira@gmail.com>
2024-02-09 07:59:09 +0300

    added file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var fullname: String ="Joy Kendi"
+    var age = 20
+    var phoneNumber="0745677890"
+    var kg: Float = 61f
+    var nationality = "kenya"
+
+    println(fullname)
+    println(age)
+    println(phoneNumber)
+    println(kg)
+    println(nationality)
+
+
+
+}
